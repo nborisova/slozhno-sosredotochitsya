5039fd4ed71eab5c122d35739fe238a4266a5bf6
Natalia Borisova <borisova.natalia.14@gmail.com>
2024-08-21 20:43:38 +0200

    верстка main

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,12 @@ const cssnano = require('cssnano');
 const htmlMinify = require('html-minifier');
 
 function serve() {
-    browserSync.init({
-      server: {
-        baseDir: './dist'
-      }
-    });
-  }
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+}
 
 function html() {
   const options = {
@@ -30,72 +30,72 @@ function html() {
     keepClosingSlash: true
   };
 
-    return gulp.src('src/**/*.html')
-          .pipe(plumber())
-          .on('data', function(file) {
-            const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
-            return file.contents = buferFile
-          })
-                  .pipe(gulp.dest('dist/'))
-                  .pipe(browserSync.reload({stream: true}));
+  return gulp.src('src/**/*.html')
+        .pipe(plumber())
+        .on('data', function(file) {
+          const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
+          return file.contents = buferFile
+        })
+                .pipe(gulp.dest('dist/'))
+                .pipe(browserSync.reload({stream: true}));
+}
+
+function css() {
+  const plugins = [
+    autoprefixer(),
+    mediaquery(),
+    cssnano()
+  ]
+
+  return gulp.src('src/styles/**/*.css')
+        .pipe(plumber())
+        .pipe(concat('bundle.css'))
+        .pipe(postcss(plugins))
+                .pipe(gulp.dest('dist/'))
+                .pipe(browserSync.reload({stream: true}));
   }
 
-  function css() {
-    const plugins = [
-      autoprefixer(),
-      mediaquery(),
-      cssnano()
-    ]
+function scripts() {
+  return gulp.src('src/scripts/**/*.js')
+        .pipe(plumber())
+                .pipe(gulp.dest('dist/scripts'))
+                .pipe(browserSync.reload({stream: true}));
+}
 
-    return gulp.src('src/styles/**/*.css')
-          .pipe(plumber())
-          .pipe(concat('bundle.css'))
-          .pipe(postcss(plugins))
-                  .pipe(gulp.dest('dist/'))
-                  .pipe(browserSync.reload({stream: true}));
-  }
+function images() {
+  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+            .pipe(gulp.dest('dist/images'))
+            .pipe(browserSync.reload({stream: true}));
+}
 
-  function scripts() {
-    return gulp.src('src/scripts/**/*.js')
-          .pipe(plumber())
-                  .pipe(gulp.dest('dist/scripts'))
-                  .pipe(browserSync.reload({stream: true}));
-  }
-
-  function images() {
-    return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-              .pipe(gulp.dest('dist/images'))
-              .pipe(browserSync.reload({stream: true}));
-  }
-
-  function fonts() {
-    return gulp.src('src/fonts/**/*')
-              .pipe(gulp.dest('dist/fonts'))
-              .pipe(browserSync.reload({stream: true}));
-  }
+function fonts() {
+  return gulp.src('src/fonts/**/*')
+            .pipe(gulp.dest('dist/fonts'))
+            .pipe(browserSync.reload({stream: true}));
+}
   
-  function clean() {
-    return del('dist');
-  }
+function clean() {
+  return del('dist');
+}
 
-  function watchFiles() {
-    gulp.watch(['src/**/*.html'], html);
-    gulp.watch(['src/styles/**/*.css'], css);
-    gulp.watch(['src/scripts/**/*.js'], scripts);
-    gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
-    gulp.watch(['src/fonts/**/*'], fonts);
-  }
+function watchFiles() {
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/styles/**/*.css'], css);
+  gulp.watch(['src/scripts/**/*.js'], scripts);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/fonts/**/*'], fonts);
+}
   
-  const build = gulp.series(clean, gulp.parallel(html, css, scripts, images, fonts));
-  const watchapp = gulp.parallel(build, watchFiles, serve);
+const build = gulp.series(clean, gulp.parallel(html, css, scripts, images, fonts));
+const watchapp = gulp.parallel(build, watchFiles, serve);
 
-  exports.html = html;
-  exports.css = css;
-  exports.scripts = scripts;
-  exports.images = images;
-  exports.fonts = fonts;
-  exports.clean = clean;
+exports.html = html;
+exports.css = css;
+exports.scripts = scripts;
+exports.images = images;
+exports.fonts = fonts;
+exports.clean = clean;
 
-  exports.build = build;
-  exports.watchapp = watchapp;
-  exports.default = watchapp;
+exports.build = build;
+exports.watchapp = watchapp;
+exports.default = watchapp;
